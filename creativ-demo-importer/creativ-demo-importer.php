dd7bc9943fd3ad19679d9c926642ed973e69bfb8
Sean Murphy <sean@iamseanmurphy.com>
2025-08-22 08:19:10 +0000

    1.3.6

--- creativ-demo-importer/creativ-demo-importer.php
+++ creativ-demo-importer/creativ-demo-importer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Creativ Demo Importer
  * Description: Creativ Themes starter sites importer.
- * Version:     1.3.5
+ * Version:     1.3.6
  * Author:      creativthemes
  * Author URI:  
  * License:     GPLv3 or later
--- creativ-demo-importer/includes/import-functions.php
+++ creativ-demo-importer/includes/import-functions.php
@@ -85,6 +85,9 @@ function creativ_demo_importer_starter_sites($starter_sites){
     elseif(get_stylesheet() == 'creativ-business-fse'){
         require plugin_dir_path(__FILE__) . 'starter-sites/creativ-business-fse-list.php';
     }
+    elseif(get_stylesheet() == 'mavix-portfolio'){
+        require plugin_dir_path(__FILE__) . 'starter-sites/mavix-portfolio-list.php';
+    }
     else{
         require plugin_dir_path(__FILE__) . 'starter-sites/default-list.php';
     }
