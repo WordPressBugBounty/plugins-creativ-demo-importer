abeae29c18b6ceeb9ad4826bb15d33bf3684c497
Sean Murphy <sean@iamseanmurphy.com>
2026-05-20 09:25:35 +0000

    1.5.4

--- creativ-demo-importer/creativ-demo-importer.php
+++ creativ-demo-importer/creativ-demo-importer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Creativ Demo Importer
  * Description: Creativ Themes starter sites importer.
- * Version:     1.5.3
+ * Version:     1.5.4
  * Author:      creativthemes
  * Author URI:  
  * License:     GPLv3 or later
--- creativ-demo-importer/includes/import-functions.php
+++ creativ-demo-importer/includes/import-functions.php
@@ -121,6 +121,9 @@ function creativ_demo_importer_starter_sites($starter_sites){
     elseif(get_stylesheet() == 'mavix-preschool'){
         require plugin_dir_path(__FILE__) . 'starter-sites/mavix-preschool-list.php';
     }
+    elseif(get_stylesheet() == 'mavix-accounting'){
+        require plugin_dir_path(__FILE__) . 'starter-sites/mavix-accounting-list.php';
+    }
     else{
         require plugin_dir_path(__FILE__) . 'starter-sites/default-list.php';
     }
